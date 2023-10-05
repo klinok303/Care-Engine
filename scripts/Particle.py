8bd49001045f4011c153bc92e73fd70b7309b567
klinok303 <egorovqatolia@gmail.com>
2023-10-05 19:19:18 +0300

    Particle Refactor

--- scripts/Particle.py
+++ scripts/Particle.py
@@ -1,19 +1,18 @@
 import ctypes
 
 
-class ParticleData(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_char_p), ("pos", ctypes.c_float * 2), ("velocity", ctypes.c_float * 2)]
+class CParticle(ctypes.Structure):
+    _fields_ = [("pos", ctypes.c_float * 2), ("velocity", ctypes.c_float * 2)]
 
 
 class Particle:
     def __init__(self, game, p_type, pos, velocity=None, frame=0):
         if velocity == None: velocity = [0.0, 0.0]
 
-        self.lib = ctypes.CDLL("./Particle.dll")
-        self.lib.ParticleUpdate.restype = ParticleData
+        self.lib = ctypes.CDLL("./libParticle.dll")
+        self.lib.ParticleUpdate.restype = CParticle
 
-        self.particle = ParticleData(bytes(p_type.encode('utf-8')), (ctypes.c_float * 2)(*list(pos)),
-                                     (ctypes.c_float * 2)(*list(velocity)))
+        self.struct = CParticle((ctypes.c_float * 2)(*list(pos)), (ctypes.c_float * 2)(*list(velocity)))
 
         self.game = game
 
@@ -24,7 +23,7 @@ class Particle:
         kill = False
         if self.animation.done: kill = True
 
-        self.particle = self.lib.ParticleUpdate(self.particle)
+        self.struct = self.lib.ParticleUpdate(self.struct)
 
         self.animation.update()
 
@@ -32,5 +31,7 @@ class Particle:
 
     def render(self, surf, offset=(0, 0)):
         img = self.animation.img()
-        surf.blit(img, (self.particle.pos[0] - offset[0] - img.get_width() // 2,
-                        self.particle.pos[1] - offset[1] - img.get_height() // 2))
+        surf.blit(img, (self.lib.ParticleRenderCalc(self.struct, (ctypes.c_float * 2)(*list(offset)),
+                                                    (ctypes.c_uint8 * 2)(*[img.get_height(), img.get_width()]), 0),
+                        self.lib.ParticleRenderCalc(self.struct, (ctypes.c_float * 2)(*list(offset)),
+                                                    (ctypes.c_uint8 * 2)(*[img.get_height(), img.get_width()]), 1)))
